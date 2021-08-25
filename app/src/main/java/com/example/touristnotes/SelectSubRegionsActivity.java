a9ddf0dd5becd12efca1bcf72f0b675e19e06470
TWaster <Draco1996@mail.ru>
2021-08-25 20:00:53 +0300

    Работа с выбором района

--- app/src/main/java/com/example/touristnotes/SelectSubRegionsActivity.java
+++ app/src/main/java/com/example/touristnotes/SelectSubRegionsActivity.java
@@ -2,16 +2,87 @@ package com.example.touristnotes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.touristnotes.JSONReaderURL.subRegionsRead;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 public class SelectSubRegionsActivity extends AppCompatActivity {
+
+    private static final String JSON_URL = "https://touristnotes.000webhostapp.com/";
+
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.select_sub_regions);
+        setContentView(R.layout.activity_main);
+        listView = (ListView) findViewById(R.id.listView);
+        loadJSONFromURL(JSON_URL);
+
+    }
+
+    private void loadJSONFromURL(String url) {
+        final ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        progressBar.setVisibility(ListView.VISIBLE);
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        progressBar.setVisibility(ListView.INVISIBLE);
+                        try {
+                            JSONObject object = new JSONObject(response);
+                            JSONArray jsonArray = object.getJSONArray("sub_regions");
+                            ArrayList<JSONObject> listItems = getArrayListFromJSONArray(jsonArray);
+
+                            ListAdapter adapter = new subRegionsRead(getApplicationContext(), R.layout.list_item, R.id.sr_name, listItems);
+                            listView.setAdapter(adapter);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+
+    private ArrayList<JSONObject> getArrayListFromJSONArray(JSONArray jsonArray) {
+        ArrayList<JSONObject> aList = new ArrayList<JSONObject>();
+        try {
+            if (jsonArray != null) {
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    aList.add(jsonArray.getJSONObject(i));
+                }
+            }
+        } catch (JSONException js) {
+            js.printStackTrace();
+        }
+        return aList;
     }
 
     public void onClick(View view) {
