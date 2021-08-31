0fa34fe7f2b2d52dc288f136748bd27295060d77
TWaster <Draco1996@mail.ru>
2021-08-31 13:28:31 +0300

    мелкие правки

--- app/src/main/java/com/example/touristnotes/MainActivity.java
+++ app/src/main/java/com/example/touristnotes/MainActivity.java
@@ -1,30 +1,10 @@
 package com.example.touristnotes;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ListAdapter;
-import android.widget.ListView;
-import android.widget.ProgressBar;
-import android.widget.Toast;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-import com.example.touristnotes.JSONReaderURL.subRegionsRead;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
