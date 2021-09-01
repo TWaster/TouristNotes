a920df1bd711260019bd825138a176c37954c18e
TWaster <Draco1996@mail.ru>
2021-09-01 18:25:57 +0300

    Переработка выгрузки Категорий

--- app/src/main/java/com/example/touristnotes/CategoryObjectActivity.java
+++ app/src/main/java/com/example/touristnotes/CategoryObjectActivity.java
@@ -2,38 +2,84 @@ package com.example.touristnotes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.app.Activity;
-import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.GridView;
-import android.widget.TextView;
-import android.app.Activity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.GridView;
-import android.widget.TextView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.touristnotes.JSONReaderURL.RegionsRead;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class CategoryObjectActivity extends AppCompatActivity {
 
-    private TextView mSelectText;
+    private static final String JSON_URL = "http://travelesnotes.ru/api/readCategories.php";
+
+    ListView listView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.category_object);
+        listView = (ListView) findViewById(R.id.listView_categories); //Выбор нужного ID ListView
+        loadJSONFromURL(JSON_URL);
+    }
 
-        GridView gridview = (GridView) findViewById(R.id.gridview);
-        gridview.setAdapter(new ImageTextAdapter(this));
+    private void loadJSONFromURL(String url) {
+        final ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar_categories);
+        progressBar.setVisibility(ListView.VISIBLE);
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        progressBar.setVisibility(ListView.INVISIBLE);
+                        try {
+                            JSONObject object = new JSONObject(response);
+                            JSONArray jsonArray = object.getJSONArray("object_type"); //Название подгружаемого объекта JSON
+                            ArrayList<JSONObject> listItems = getArrayListFromJSONArray(jsonArray);
 
-        // gridview.setOnItemClickListener(gridviewOnItemClickListener);
+                            ListAdapter adapter = new RegionsRead(getApplicationContext(), R.layout.list_item, R.id.sr_name, listItems);
+                            listView.setAdapter(adapter);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+
+    private ArrayList<JSONObject> getArrayListFromJSONArray(JSONArray jsonArray) {
+        ArrayList<JSONObject> aList = new ArrayList<JSONObject>();
+        try {
+            if (jsonArray != null) {
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    aList.add(jsonArray.getJSONObject(i));
+                }
+            }
+        } catch (JSONException js) {
+            js.printStackTrace();
+        }
+        return aList;
     }
 
     public void onClick(View view) {
