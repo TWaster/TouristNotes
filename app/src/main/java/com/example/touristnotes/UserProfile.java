1ba58c613a11320db4149bb8e7634f17e27cd1fe
NikitaOsik97 <78144505+NikitaOsik97@users.noreply.github.com>
2021-09-03 00:30:34 +0700

    выгрузка на обсуждение страницы профиля

---
+++ app/src/main/java/com/example/touristnotes/UserProfile.java
@@ -0,0 +1,18 @@
+package com.example.touristnotes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class UserProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_profile);
+    }
+
+    public void onClick(View view) {
+    }
+}
