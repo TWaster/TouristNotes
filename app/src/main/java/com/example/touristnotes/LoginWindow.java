2da85c0f0f8642a15b6f9a99629b1b761ec7743d
NikitaOsik97 <78144505+NikitaOsik97@users.noreply.github.com>
2021-09-06 20:51:05 +0700

    выгрузка окно входа (login_window), выгрузка часичных наработок activity объекта (object),

---
+++ app/src/main/java/com/example/touristnotes/LoginWindow.java
@@ -0,0 +1,14 @@
+ package com.example.touristnotes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginWindow extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_window);
+    }
+}
---
+++ app/src/main/java/com/example/touristnotes/ObjectActivity.java
@@ -0,0 +1,18 @@
+package com.example.touristnotes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class ObjectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.object);
+    }
+
+    public void onClick(View view) {
+    }
+}
