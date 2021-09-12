56de8f60a752999a31203485d5334a4ac7df07ae
NikitaOsik97 <78144505+NikitaOsik97@users.noreply.github.com>
2021-09-12 22:38:11 +0700

    выгрузка с внедрением предварительного дизайна в login_window и main_activity

--- app/src/main/java/com/example/touristnotes/MainActivity.java
+++ app/src/main/java/com/example/touristnotes/MainActivity.java
@@ -5,12 +5,36 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.models.SlideModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageSlider imageSlider = findViewById(R.id.image_slider);
+        List<SlideModel> slideModels = new ArrayList<>();
+
+
+        slideModels.add(new SlideModel("http://travelesnotes.ru/images/preview/1.png","Image 1"));
+        slideModels.add(new SlideModel("http://travelesnotes.ru/images/preview/2.png","Image 2"));
+        slideModels.add(new SlideModel("http://travelesnotes.ru/images/preview/3.png","Image 3"));
+
+
+
+
+
+        imageSlider.setImageList(slideModels, true);
+
+
     }
 
     public void onClick(View view) {
