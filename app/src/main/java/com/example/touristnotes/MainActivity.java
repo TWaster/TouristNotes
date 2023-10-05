2d8d5fc9354db7422e418917ffd85b0cc8ab7ddf
TWaster <Draco1996@mail.ru>
2023-10-05 19:47:01 +0300

    Подгрузка аватара пользователя реализована  (06.10.2023)

--- app/src/main/java/com/example/touristnotes/MainActivity.java
+++ app/src/main/java/com/example/touristnotes/MainActivity.java
@@ -3,16 +3,11 @@ package com.example.touristnotes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.graphics.Typeface;
-import android.widget.Toast;
-
 
 import com.denzcoskun.imageslider.ImageSlider;
 import com.denzcoskun.imageslider.constants.ScaleTypes;
@@ -21,23 +16,19 @@ import com.denzcoskun.imageslider.models.SlideModel;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
-import java.util.List;
-
 
 public class MainActivity extends AppCompatActivity {
-
     //Предустановка Слайдера на Главной странице
     //*****************************************************************************************
     //В последствии настроить отображение слайдера на некоторые выборки из популярного контента
     //*****************************************************************************************
-    private ImageView userImageView;
     ImageSlider imageSlider;
     @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        imageSlider = (ImageSlider) findViewById(R.id.image_slider);
+        imageSlider = findViewById(R.id.image_slider);
         ArrayList<SlideModel> slideModels = new ArrayList<>();
 
         slideModels.add(new SlideModel("http://travelesnotes.ru/images/preview/1.png", ScaleTypes.FIT));
@@ -45,13 +36,11 @@ public class MainActivity extends AppCompatActivity {
         slideModels.add(new SlideModel("http://travelesnotes.ru/images/preview/3.png", ScaleTypes.FIT));
         imageSlider.setImageList(slideModels);
 
-        TextView user_level = (TextView) findViewById(R.id.profile_level);
+        TextView user_level = findViewById(R.id.profile_level);
         user_level.setText(getIntent().getStringExtra("u_level") + " Ур.");
-        TextView user_set_region = (TextView) findViewById(R.id.CheckedRegion);
+        TextView user_set_region = findViewById(R.id.CheckedRegion);
         user_set_region.setText(getIntent().getStringExtra("u_region"));
-
-        userImageView = findViewById(R.id.profile_image);
-        Toast.makeText(MainActivity.this, getIntent().getStringExtra("u_avatar"), Toast.LENGTH_SHORT).show();
+        ImageView userImageView = findViewById(R.id.profile_image);
         Picasso.get().load(getIntent().getStringExtra("u_avatar")).into(userImageView);
     }
 
