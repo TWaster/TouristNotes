8346b2bb8a547081ece61f8d3262cac37f69627c
TWaster <Draco1996@mail.ru>
2023-11-23 19:22:01 +0300

    Информация о сложности объекта добавлена, так же добавлена иконка help для обьяснения уровней сложности (надо сделать всплывающую подсказку). Так же надо дорабатывать ListView для отображения маркера в листе объектов(23.11.2023)

--- app/src/main/java/com/example/touristnotes/ObjectActivity.java
+++ app/src/main/java/com/example/touristnotes/ObjectActivity.java
@@ -6,9 +6,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -57,16 +59,41 @@ public class ObjectActivity extends AppCompatActivity {
                         TextView ObjectRating = findViewById(R.id.ratingValue);
                         ImageView ImageObject = findViewById(R.id.image_object);
                         ImageView ObjectMarked = findViewById(R.id.check_object);
+                        Button MarkedObject = findViewById(R.id.markedobject);
+                        ImageView PB_Dificulty = findViewById(R.id.PB_Dificulty);
 
                         //Описание воводов в точки
                         NameObject.setText(objectInfo.getName());
                         ObjDescription.setText(objectInfo.getInfo());
                         Picasso.get().load(objectInfo.getImage()).into(ImageObject);
                         ObjectRating.setText(objectInfo.getRating());
+                        //Прогресс бар вывода сложности объекта
+                        switch (objectInfo.getDifficulty()){
+                            case "1":
+                                PB_Dificulty.setImageResource(R.drawable.pb_1);
+                                break;
+                            case "2":
+                                PB_Dificulty.setImageResource(R.drawable.pb_2);
+                                break;
+                            case "3":
+                                PB_Dificulty.setImageResource(R.drawable.pb_3);
+                                break;
+                            case "4":
+                                PB_Dificulty.setImageResource(R.drawable.pb_4);
+                                break;
+                            case "5":
+                                PB_Dificulty.setImageResource(R.drawable.pb_5);
+                                break;
+                        }
+
+
                         if (Objects.equals(response.body().getMarked(), "1")) {
                             ObjectMarked.setVisibility(View.VISIBLE);
-                        } else
-                        {ObjectMarked.setVisibility(View.INVISIBLE);}
+                            MarkedObject.setVisibility(View.INVISIBLE);
+                        } else {
+                            ObjectMarked.setVisibility(View.INVISIBLE);
+                            MarkedObject.setVisibility(View.VISIBLE);
+                        }
 
                         //Данные для передачи
                         editor.putString(OBJECT_ID, objectInfo.getId());
